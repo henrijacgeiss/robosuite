863f4f191680e196aa919f8ed59c3da03223ed7e
henrijacgeiss <henri.geiss@uibk.ac.at>
2024-12-04 09:22:05 +0100

    Update setup.py

--- setup.py
+++ setup.py
@@ -16,6 +16,12 @@ setup(
     packages=[package for package in find_packages() if package.startswith("robosuite")],
     install_requires=[
         "numpy>=1.13.3",
+        "numba>=0.49.1",
+        "scipy>=1.2.3",
+        "Pillow",
+        "opencv-python",
+        "pynput",
+        "termcolor",
     ],
     eager_resources=["*"],
     include_package_data=True,
