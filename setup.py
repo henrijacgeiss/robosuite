86f62f8cfc12c465d9b6955f8d401f611a9ba353
Henri-Jacques Geiss <henri.geiss@uibk.ac.at>
2024-12-05 15:37:52 +0100

    Give up headles rendering for now

--- setup.py
+++ setup.py
@@ -20,7 +20,7 @@ setup(
         "scipy>=1.2.3",
         "mujoco>=2.3.0",
         "Pillow",
-        "opencv-python-headless",
+        "opencv-python",
         "termcolor",
     ],
     eager_resources=["*"],
